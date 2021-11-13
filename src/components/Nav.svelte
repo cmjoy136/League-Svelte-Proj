9d6074e62180c4cd968c3a91015d1ef9942d160f
Chris <cmjoy136@gmail.com>
2021-11-13 15:35:38 -0500

    initialized svelte repo and basic setup

---
+++ src/components/Nav.svelte
@@ -0,0 +1,20 @@
+  
+  <style>
+      .nav-container{
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+      }
+    .nav-bar {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+    }
+  </style>
+<div class="nav-container">
+  <ul class="nav-bar">
+    <li>Home</li>
+    <li>Login</li>
+    <li>Profile</li>
+  </ul>
+</div>
